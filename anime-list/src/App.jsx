b5c924c6eaad3dae2afb44ab405e34399c84d6ab
Alvarizqi09 <alvarizki80@students.unnes.ac.id>
2023-10-20 20:34:43 +0700

    side bar

--- anime-list/src/App.jsx
+++ anime-list/src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./components/Home";
 import AnimeDetail from "./components/AnimeDetail/AnimeDetail";
-import Watchlist from "./components/WatchList/Watchlist";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/anime/:category" element={<AnimeDetail />} />
-          <Route exact path="/watchlist" element={<Watchlist />} />
         </Routes>
       </>
     </HashRouter>
--- anime-list/src/components/Sidebar/Sidebar.jsx
+++ anime-list/src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,21 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function Sidebar({popularAnime }) {
+function Sidebar({ topAnime }) {
   return (
     <div>
       <aside className="w-64 m-5">
-        <nav>
-          <h3 className="text-2xl font-bold text-gray-800 mb-4">Popular Anime</h3>
+      <nav>
+          <h3 className="text-2xl font-bold text-gray-800 mb-4">Top Anime</h3>
           <div>
-            {popularAnime &&
-              popularAnime.map((anime, index) => (
-                <div className="flex items-center p-4 bg-white shadow-md rounded-lg mb-4" key={anime.mal_id}>
-                  <span className="text-2xl text-gray-600">{index + 1}</span>
+            {topAnime &&
+              topAnime.map((anime, index) => (
+                <div className="flex items-center p-4 bg-white shadow-md rounded-lg mb-4"  key={anime.mal_id}>
+                  <span className="text-2xl text-gray-600">
+                    {index + 1}
+                  </span>
                   <Link to={`/anime/${anime.mal_id}`} className="flex items-center space-x-2">
-                    <img src={anime.images.jpg.small_image_url} alt="popular" className="w-15 h-15 m-2 rounded-md" />
+                    <img src={anime.images.jpg.small_image_url} alt="top" className="w-15 h-15 m-2 rounded-md" />
                     <span className="text-center text-gray-800">{anime.title}</span>
                   </Link>
                 </div>
@@ -26,7 +28,7 @@ function Sidebar({popularAnime }) {
 }
 
 Sidebar.propTypes = {
-  popularAnime: PropTypes.array,
+  topAnime : PropTypes.array,
 };
 
 export default Sidebar;
