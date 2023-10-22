22ff7c49a96da0997d67c5073016f5a0b7ec49e8
Alvarizqi09 <alvarizki80@students.unnes.ac.id>
2023-10-23 03:54:54 +0700

    fixing auth

--- anime-list/src/App.jsx
+++ anime-list/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import AnimeDetail from "./components/AnimeDetail/AnimeDetail";
 import LandingPage from "./components/LandingPage/LandingPage";
@@ -8,17 +8,15 @@ import Login from "./components/Login/Login";
 function App() {
 
   return (
-    <HashRouter>
-      <>
-        <Routes>
-          <Route exact path="/listanime" element={<Home />} />
-          <Route exact path="/customerservices" element={<CustomerServices />} />
-          <Route exact path="/" element={<LandingPage/>} />
-          <Route exact path="/login" element={<Login/>} />
-          <Route exact path="/anime/:category" element={<AnimeDetail />} />
-        </Routes>
-      </>
-    </HashRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/listanime" element={<Home />} />
+        <Route path="/customerservices" element={<CustomerServices />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/anime/:category" element={<AnimeDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
