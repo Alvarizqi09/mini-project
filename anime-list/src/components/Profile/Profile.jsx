3fee4cd1932549a6501bbd50e10c71b5519f00dc
Alvarizqi09 <alvarizki80@students.unnes.ac.id>
2023-10-25 22:07:44 +0700

    fixing login

--- anime-list/src/components/Profile/Profile.jsx
+++ anime-list/src/components/Profile/Profile.jsx
@@ -82,110 +82,110 @@ const Profile = () => {
         <h2 className="text-2xl font-semibold mb-4 text-white">Your Profile</h2>
       </div>
       <div className="max-w-md bg-white rounded-lg p-4 shadow-lg mx-auto text-center">
-      <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Avatar</label>
-          {isEditing ? (
-            <input
-              type="file"
-              name="avatar"
-              accept="image/*"
-              onChange={handleImageChange}
-              className="border rounded w-full p-2"
-            />
-          ) : (
-            <div className="mb-2">
-              <img src={userData.avatar} alt="Avatar" className="w-20 h-20 mx-auto rounded-full" />
-            </div>
-          )}
-        </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Full Name</label>
+            <label className="block text-gray-700 text-sm font-bold mb-2">Avatar</label>
+            {isEditing ? (
+              <input
+                type="file"
+                name="avatar"
+                accept="image/*"
+                onChange={handleImageChange}
+                className="border rounded w-full p-2"
+              />
+            ) : (
+              <div className="mb-2">
+                <img src={userData.avatar} alt="Avatar" className="w-20 h-20 mx-auto rounded-full" />
+              </div>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Full Name</label>
+            {isEditing ? (
+              <input
+                type="text"
+                name="fullName"
+                value={userData.fullName}
+                onChange={handleInputChange}
+                className="border rounded w-full p-2"
+              />
+            ) : (
+              <p className="text-lg   font-semibold mb-2">{userData.fullName}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Username</label>
+            {isEditing ? (
+              <input
+                type="text"
+                name="username"
+                value={userData.username}
+                onChange={handleInputChange}
+                className="border rounded w-full p-2"
+              />
+            ) : (
+              <p className="text-lg font-semibold mb-2">@{userData.username}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Birthdate</label>
+            {isEditing ? (
+              <input
+                type="date"
+                name="birthdate"
+                value={userData.birthdate}
+                onChange={handleInputChange}
+                className="border rounded w-full p-2"
+              />
+            ) : (
+              <p className="text-lg font-semibold mb-2">{userData.birthdate}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Gender</label>
+            {isEditing ? (
+              <select
+                name="gender"
+                value={userData.gender}
+                onChange={handleInputChange}
+                className="border rounded w-full p-2"
+              >
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            ) : (
+              <p className="text-lg   font-semibold mb-2">{userData.gender}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">City</label>
+            {isEditing ? (
+              <input
+                type="text"
+                name="city"
+                value={userData.city}
+                onChange={handleInputChange}
+                className="border rounded w-full p-2"
+              />
+            ) : (
+              <p className="text-lg   font-semibold mb-2">{userData.city}</p>
+            )}
+          </div>
           {isEditing ? (
-            <input
-              type="text"
-              name="fullName"
-              value={userData.fullName}
-              onChange={handleInputChange}
-              className="border rounded w-full p-2"
-            />
+            <button onClick={handleSave} className="bg-blue-500 text-white mb-4 p-2 rounded-md">
+              Save
+            </button>
           ) : (
-            <p className="text-lg   font-semibold mb-2">{userData.fullName}</p>
+            <button onClick={() => setIsEditing(true)} className="bg-blue-500 mb-4 text-white p-2 rounded-md">
+              Edit Profile
+            </button>
           )}
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Username</label>
-          {isEditing ? (
-            <input
-              type="text"
-              name="username"
-              value={userData.username}
-              onChange={handleInputChange}
-              className="border rounded w-full p-2"
-            />
-          ) : (
-            <p className="text-lg   font-semibold mb-2">@{userData.username}</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Birthdate</label>
-          {isEditing ? (
-            <input
-              type="date"
-              name="birthdate"
-              value={userData.birthdate}
-              onChange={handleInputChange}
-              className="border rounded w-full p-2"
-            />
-          ) : (
-            <p className="text-lg   font-semibold mb-2">{userData.birthdate}</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Gender</label>
-          {isEditing ? (
-            <select
-              name="gender"
-              value={userData.gender}
-              onChange={handleInputChange}
-              className="border rounded w-full p-2"
-            >
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Other</option>
-            </select>
-          ) : (
-            <p className="text-lg   font-semibold mb-2">{userData.gender}</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">City</label>
-          {isEditing ? (
-            <input
-              type="text"
-              name="city"
-              value={userData.city}
-              onChange={handleInputChange}
-              className="border rounded w-full p-2"
-            />
-          ) : (
-            <p className="text-lg   font-semibold mb-2">{userData.city}</p>
-          )}
-        </div>
-        {isEditing ? (
-          <button onClick={handleSave} className="bg-blue-500 text-white mb-4 p-2 rounded-md">
-            Save
-          </button>
-        ) : (
-          <button onClick={() => setIsEditing(true)} className="bg-blue-500 mb-4 text-white p-2 rounded-md">
-            Edit Profile
-          </button>
-        )}
-        <div>
-          <button onClick={handleDelete} className="bg-red-500 text-white mb-4 p-2 rounded-md">
-            Delete Profile
-          </button>
-          <Logout />
-        </div>
+          <div>
+            <button onClick={handleDelete} className="bg-red-500 text-white mb-4 p-2 rounded-md">
+              Delete Profile
+            </button>
+            <Logout />
+          </div>
       </div>
     </div>
   );
